420e579eb01f222493b689ceb4aa222df8e55e6c
James Lovejoy <james.lovejoy2@gmail.com>
2021-02-03 13:15:29 -0600

    added navbar with signout capabilites, placed navbar onto homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 //added to complete set-up of amplify project
 import { Amplify } from "aws-amplify";
 import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>AWS Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Navbar />
 
       <main className={styles.main}>
         <h1 className={styles.title}>
