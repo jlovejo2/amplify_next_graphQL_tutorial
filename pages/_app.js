62a98668853710e9dd1e6434e5abaf45e891b96e
James Lovejoy <james.lovejoy2@gmail.com>
2021-02-03 13:07:26 -0600

    added amplifyAuthenticator to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 //added to complete set-up of amplify project
+import { Amplify } from "aws-amplify";
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react";
 import awsExports from "../src/aws-exports";
 
 Amplify.configure({...awsExports, ssr: true });
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AmplifyAuthenticator>
+      <Component {...pageProps} />
+    </AmplifyAuthenticator> 
+  );
 }
 
 export default MyApp
