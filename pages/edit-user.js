23c8755ca70cdd70a1e64b41cfe1ab228032fa75
James Lovejoy <james.lovejoy2@gmail.com>
2021-02-03 17:38:51 -0600

    added serversideProps fetching of user authentification

--- pages/edit-user.js
+++ pages/edit-user.js
@@ -1,14 +1,51 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
+import {getUser} from "../src/graphql/queries";
 import { createUser, updateUser } from "../src/graphql/mutations";
+import {withSSRContext} from "aws-amplify";
 import { API } from "@aws-amplify/api";
 import { Auth } from  "@aws-amplify/auth";
 import Navbar from "../components/Navbar";
 
-const EditUser = () => {
-    const [firstName, setFirstName] = useState('');
-    const [secondName, setSecondName] = useState('');
-    const [description, setDescription] = useState('');
+export async function getServerSideProps({req, res}) {
+    const {Auth, API} = withSSRContext({req});
+    try {
+        const user = await Auth.currentAuthenticatedUser();
+        const response = await API.graphql({
+            query: getUser,
+            variables: {id: user.attributes.sub},
+        });
+        if (response.data.getUser) {
+            return {
+                props: {
+                    mode: "EDIT",
+                    user: response.data.getUser,
+                    error: false,
+                },
+            };
+        } else {
+            return {
+                props: {
+                    mode: "ADD",
+                    error: false,
+                },
+            };
+        }
+    } catch (err) {
+        console.log('error: ', err);
+        return {
+            props: {
+                error: true,
+            },
+        };
+    }
+}
+
+const EditUser = ({ error, user, mode } ) => {
+
+    const [firstName, setFirstName] = useState(mode === 'EDIT' ? user.firstName : '');
+    const [secondName, setSecondName] = useState(mode === 'EDIT' ? user.lastName : '');
+    const [description, setDescription] = useState(mode === 'EDIT' ? user.description : '');
 
     const submitHandler = async (event) => {
         event.preventDefault();
@@ -18,7 +55,7 @@ const EditUser = () => {
 
         try {
             const result = await API.graphql({
-                query: createUser,
+                query: mode === 'EDIT' ? updateUser : createUser,
                 variables: {
                     input: {
                         id: currentUser.attributes.sub,
@@ -29,10 +66,20 @@ const EditUser = () => {
                 }
             })
             console.log(result)
+            window.location.href = "/"
         } catch (err) {
             console.log(err);
         }
     };
+
+    if (error) {
+        return (
+            <div>
+                <Navbar />
+                <h1>Something went wrong. Please try again later.</h1>
+            </div>
+        )
+    }
 
     return (
         <div className="d-flex flex-column justify-content-center w-100 h-100">
